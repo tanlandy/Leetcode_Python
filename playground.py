adca257697442ac68296d6d8e3e5849df3e471e3
Kunlin <tanlandy6@gmail.com>
2022-04-29 14:36:00 -0700

    sort

--- playground.py
+++ playground.py
@@ -1,3 +1,19 @@
-s = "123456"
+def bubbleSort(nums):
+    for i in range(len(nums)):
+        swapped = False
 
-print(s[0:2])
+        for j in range(1, len(nums) - i):
+            if nums[j - 1] > nums[j]:
+                nums[j - 1], nums[j] = nums[j], nums[j - 1]
+                swapped = True
+            
+        if not swapped:
+            return nums
+    
+    return nums
+
+nums=[1, 4, 2, 4, 5, 2, 5, 22]
+
+bubbleSort(nums)
+
+print(nums)
